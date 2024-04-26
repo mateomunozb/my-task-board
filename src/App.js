d979ed4f9bc12c4b4dc08dadf37c13a790d3df7c
Mateo <mateo.munoz@masivian.com>
2024-04-26 18:58:08 -0500

    Create project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el curso de introduccion a React.js', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LAALALAALALA', completed: true }
+];
+
+function App() {
+  return (
+    <>
+      <TodoCounter completed={16} total={25}/>
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map((todo, i) => (
+          <TodoItem key={i} text={todo.text} completed={todo.completed}/>
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+    </>
+  );
+}
+
+export default App;
